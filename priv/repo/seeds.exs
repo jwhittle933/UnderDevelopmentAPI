6f34770644e0e83e4a36ce8783b8d7804fdeeeb1
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-04-27 14:38:20 -0400

    attempts

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -26,85 +26,144 @@ Repo.delete_all(Accounts.User)
   Destructure {:ok, ...} for Ecto.build_assoc/3
   Accounts.create_user for hashing password
 """
-{:ok, admin_cred} = %{
+admin_cred = %{
   name: "admin", 
   email: "admin@admin.com", 
-  password: "administrator", 
-  admin: true
-} |> Accounts.create_user
+  password_hash: Bcrypt.add_hash("administrator")[:password_hash], 
+  admin: true,
+} |> Repo.insert!
 
-{:ok, jw} = %{
+jw = %{
   name: "Jonathan Whittle", 
   email: "jonathan.m.whittle@gmail.com", 
-  password: "jwhittle", 
-  admin: true
-} |> Accounts.create_user
+  password_hash: Bcrypt.add_hash("jwhittle")[:password_hash], 
+  admin: true,
+  posts: [
+    %{
+      body: "Lalalalal",
+      title: "Hello World",
+      featured_image: "https://s3.aws.com/featuredImage/8dklf-2kdf",
+      comments: [
+        %{
+          comment: "Hooray!",
+          name: "Joe Joe Fo Sho"
+        },
+        %{
+          comment: "What a great Post",
+          name: "Paul Schwifty"
+        },
+        %{
+          comment: "I don't aggree. Wanna fight about it?",
+          name: "Woke-a-moke"
+        }
+      ]
+    },
+    %{
+      body: "Second Post",
+      title: "Hello World",
+      featured_image: "https://s3.aws.com/featuredImage/123ldfs",
+      comments: [
+        %{
+          comment: "Hooray!",
+          name: "Joe Joe Fo Sho"
+        },
+        %{
+          comment: "What a great Post",
+          name: "Paul Schwifty"
+        },
+        %{
+          comment: "I don't aggree. Wanna fight about it?",
+          name: "Woke-a-moke"
+        }
+      ]
+    },
+    %{
+      body: "third post",
+      title: "Hello World",
+      featured_image: "https://s3.aws.com/featuredImage/12",
+      comments: [
+        %{
+          comment: "Hooray!",
+          name: "Joe Joe Fo Sho"
+        },
+        %{
+          comment: "What a great Post",
+          name: "Paul Schwifty"
+        },
+        %{
+          comment: "I don't aggree. Wanna fight about it?",
+          name: "Woke-a-moke"
+        }
+      ]
+    }
+  ]
+} |> Repo.insert!
 
 """
   Lists of posts |> Enum.each
   Ecto.build_assoc/3 |> Repo.insert!/1
 """
-jw_posts = [
-  %{
-    body: "Lalalalal",
-    title: "Hello World",
-    featured_image: "https://s3.aws.com/featuredImage/8dklf-2kdf",
-    comments: [
-      %{
-        comment: "Hooray!",
-        name: "Joe Joe Fo Sho"
-      },
-      %{
-        comment: "What a great Post",
-        name: "Paul Schwifty"
-      },
-      %{
-        comment: "I don't aggree. Wanna fight about it?",
-        name: "Woke-a-moke"
-      }
-    ]
-  },
-  %{
-    body: "Second Post",
-    title: "Hello World",
-    featured_image: "https://s3.aws.com/featuredImage/123ldfs",
-    comments: [
-      %{
-        comment: "Hooray!",
-        name: "Joe Joe Fo Sho"
-      },
-      %{
-        comment: "What a great Post",
-        name: "Paul Schwifty"
-      },
-      %{
-        comment: "I don't aggree. Wanna fight about it?",
-        name: "Woke-a-moke"
-      }
-    ]
-  },
-  %{
-    body: "third post",
-    title: "Hello World",
-    featured_image: "https://s3.aws.com/featuredImage/12",
-    comments: [
-      %{
-        comment: "Hooray!",
-        name: "Joe Joe Fo Sho"
-      },
-      %{
-        comment: "What a great Post",
-        name: "Paul Schwifty"
-      },
-      %{
-        comment: "I don't aggree. Wanna fight about it?",
-        name: "Woke-a-moke"
-      }
-    ]
-  }
-] |> Enum.each(fn(post) -> 
-  Ecto.build_assoc(jw, :posts, post) |> Repo.insert!
-end)
+# jw_posts = [
+#   %{
+#     body: "Lalalalal",
+#     title: "Hello World",
+#     featured_image: "https://s3.aws.com/featuredImage/8dklf-2kdf",
+#     comments: [
+#       %{
+#         comment: "Hooray!",
+#         name: "Joe Joe Fo Sho"
+#       },
+#       %{
+#         comment: "What a great Post",
+#         name: "Paul Schwifty"
+#       },
+#       %{
+#         comment: "I don't aggree. Wanna fight about it?",
+#         name: "Woke-a-moke"
+#       }
+#     ]
+#   },
+#   %{
+#     body: "Second Post",
+#     title: "Hello World",
+#     featured_image: "https://s3.aws.com/featuredImage/123ldfs",
+#     comments: [
+#       %{
+#         comment: "Hooray!",
+#         name: "Joe Joe Fo Sho"
+#       },
+#       %{
+#         comment: "What a great Post",
+#         name: "Paul Schwifty"
+#       },
+#       %{
+#         comment: "I don't aggree. Wanna fight about it?",
+#         name: "Woke-a-moke"
+#       }
+#     ]
+#   },
+#   %{
+#     body: "third post",
+#     title: "Hello World",
+#     featured_image: "https://s3.aws.com/featuredImage/12",
+#     comments: [
+#       %{
+#         comment: "Hooray!",
+#         name: "Joe Joe Fo Sho"
+#       },
+#       %{
+#         comment: "What a great Post",
+#         name: "Paul Schwifty"
+#       },
+#       %{
+#         comment: "I don't aggree. Wanna fight about it?",
+#         name: "Woke-a-moke"
+#       }
+#     ]
+#   }
+# ] |> Enum.each(fn(post) -> 
+#   Ecto.build_assoc(jw, :posts, post) |> Repo.insert!
+# end)
 
 # comments = [
 #   %{
